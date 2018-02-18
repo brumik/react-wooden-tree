bb9d28f18ea3f3ddb73883e725e3ab9148586285
Levente Berky <levente.berky@gmail.com>
2018-02-18 11:47:08 +0100

    Basic Benchmark test finalized somehow

--- src/Generator.tsx
+++ src/Generator.tsx
@@ -19,6 +19,6 @@ function generate_object(size: number): NodeProps[] {
 export function generator(): NodeProps {
     return {
         text: 'Root',
-        nodes: generate_object(27)
+        nodes: generate_object(100)
     };
 }
