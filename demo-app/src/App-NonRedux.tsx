42037e97ea15dba88274fe8e7f20a0e7e8585e26
Levente Berky <lberky@redhat.com>
2019-04-29 09:02:50 +0300

    Remived data initialization redundancy with constructor

--- demo-app/src/App-NonRedux.tsx
+++ demo-app/src/App-NonRedux.tsx
@@ -6,7 +6,6 @@ import { flat_lazy_children, generator } from './Generator';
 
 interface AppState {
     tree: TreeDataType;
-    commandHistory: (CommandQueueType & {key: Number})[];
 }
 
 const actionMapper: {[key: string]: (node: NodeProps, value: any) => NodeProps} = {
@@ -19,16 +18,23 @@ const actionMapper: {[key: string]: (node: NodeProps, value: any) => NodeProps} 
 };
 
 export class AppNonRedux extends React.Component<{}, AppState> {
-    state = {
-        tree: Tree.convertHierarchicalTree(Tree.initHierarchicalTree(generator())),
-        commandHistory: [] as (CommandQueueType & {key: Number})[],
-    };
-
     // This is needed. At too fast changes (multiple changes before the state changes)
     // we may lose some data, as we update the same node multiple times, but the second one
     // overrides the first change. Happening with lazy load loading icon.
-    private tree = Tree.convertHierarchicalTree(Tree.initHierarchicalTree(generator()));
-    private uKey: number = 0;
+    private tree: TreeDataType;
+
+    /**
+     * Constructor.
+     * @param {{}} props
+     */
+    constructor(props: {}) {
+        super(props);
+
+        this.tree = Tree.convertHierarchicalTree(Tree.initHierarchicalTree(generator()));
+        this.state = {
+            tree: this.tree,
+        };
+    }
 
     /**
      * The callback function for changing data in the tree.
@@ -49,11 +55,6 @@ export class AppNonRedux extends React.Component<{}, AppState> {
             } else if ( command.type === ActionTypes.ADD_NODES ) {
                 this.tree = Tree.addNodes(this.tree, command.value);
             }
-
-            // Only to display the command history. Not needed for the tree.
-            let newHistory = this.state.commandHistory;
-            newHistory.push({...command, key: this.uKey++});
-            this.setState({commandHistory: newHistory});
         }
         this.setState({tree: this.tree});
     }
@@ -72,56 +73,21 @@ export class AppNonRedux extends React.Component<{}, AppState> {
 
     render() {
         return (
-            <div
-                className="App"
-                style={{
-                    display: 'flex'
-                }}
-            >
-                <div
-                    style={{
-                        flex: '50%'
+            <div>
+                <Tree
+                    hierarchicalCheck={true}
+                    showCheckbox={true}
+                    multiSelect={false}
+                    preventDeselect={true}
+                    allowReselect={true}
+                    checkboxFirst={true}
+                    nodeIcon={'fa fa-fw fa-circle'}
+                    data={this.state.tree}
+                    callbacks={{
+                        onDataChange: this.onDataChange,
+                        lazyLoad: this.lazyLoad
                     }}
-                >
-                    <Tree
-                        hierarchicalCheck={true}
-                        showCheckbox={true}
-                        multiSelect={false}
-                        preventDeselect={true}
-                        allowReselect={true}
-                        checkboxFirst={true}
-                        nodeIcon={'fa fa-fw fa-circle'}
-                        data={this.state.tree}
-                        callbacks={{
-                            onDataChange: this.onDataChange,
-                            lazyLoad: this.lazyLoad
-                        }}
-                    />
-                </div>
-                <table
-                    style={{
-                        flex: '50%'
-                    }}
-                >
-                    <thead>
-                    <tr>
-                        <th colSpan={3}><h3>Action History</h3></th>
-                    </tr>
-                    <tr>
-                        <th>NodeId</th>
-                        <th>Action Type</th>
-                        <th>Passed Value</th>
-                    </tr>
-                    </thead>
-                    <tbody style={{textAlign: 'center'}}>
-                    {this.state.commandHistory.map(element =>
-                        <tr key={element.key.toString()}>
-                            <td>{element.nodeId}</td>
-                            <td>{element.type}</td>
-                            <td>{JSON.stringify(element.value)}</td>
-                        </tr>)}
-                    </tbody>
-                </table>
+                />
             </div>
         );
     }
